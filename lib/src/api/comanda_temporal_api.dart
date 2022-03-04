19c4e017db10e90f31d91701b58ce8e7fbc3f3cd
anked10 <angelo.anked@gmail.comm>
2022-03-04 17:13:05 -0500

    bababab

--- lib/src/api/comanda_temporal_api.dart
+++ lib/src/api/comanda_temporal_api.dart
@@ -14,7 +14,7 @@ class ComandaTemporalApi {
 
     try {
       final _listProducto =
-          await _comandaTemporalDatabase.obtenerDetalleComandaPorIdProducto(producto.idProducto, despacho, _preferences.idEnviarEnComanda);
+          await _comandaTemporalDatabase.obtenerDetalleComandaPorIdProducto(producto.idProducto!, despacho, _preferences.idEnviarEnComanda);
       if (_listProducto.length > 0) {
         DetalleComandaTemporalModel detalle = DetalleComandaTemporalModel();
         detalle.id = _listProducto[0].id;
@@ -22,10 +22,10 @@ class ComandaTemporalApi {
         detalle.nombreProducto = producto.productoNombre;
         detalle.idProducto = producto.idProducto;
         detalle.fotoProducto = producto.productoFoto;
-        detalle.cantidad = (cantidad + int.parse(_listProducto[0].cantidad)).toString();
+        detalle.cantidad = (cantidad + int.parse(_listProducto[0].cantidad!)).toString();
 
         detalle.subtotal = producto.productoPrecio;
-        detalle.totalDetalle = (double.parse(precioTotal) + double.parse(_listProducto[0].totalDetalle)).toStringAsFixed(2);
+        detalle.totalDetalle = (double.parse(precioTotal) + double.parse(_listProducto[0].totalDetalle!)).toStringAsFixed(2);
         detalle.observaciones = observaciones;
         detalle.despacho = despacho;
         final res = await _comandaTemporalDatabase.updateDetallePorIdComandaDetalle(detalle);
@@ -68,8 +68,8 @@ class ComandaTemporalApi {
   Future<ResultApiModel> updateDetalle(DetalleComandaTemporalModel dato, int cantidad) async {
     final ResultApiModel result = ResultApiModel();
     try {
-      if (int.parse(dato.cantidad) == 1 && cantidad < 0) {
-        final res = await _comandaTemporalDatabase.deleteDetalleTemporalPorId(dato.id);
+      if (int.parse(dato.cantidad!) == 1 && cantidad < 0) {
+        final res = await _comandaTemporalDatabase.deleteDetalleTemporalPorId(dato.id!);
         if (res > 0) {
           result.code = 1;
           result.message = 'Operación exitosa';
@@ -80,8 +80,8 @@ class ComandaTemporalApi {
           return result;
         }
       } else {
-        int newCant = int.parse(dato.cantidad) + cantidad;
-        double total = double.parse(dato.totalDetalle) + (cantidad * double.parse(dato.subtotal));
+        int newCant = int.parse(dato.cantidad!) + cantidad;
+        double total = double.parse(dato.totalDetalle!) + (cantidad * double.parse(dato.subtotal!));
         DetalleComandaTemporalModel detalle = DetalleComandaTemporalModel();
         detalle.id = dato.id;
         detalle.idMesa = dato.idMesa;

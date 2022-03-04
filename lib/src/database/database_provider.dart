ff32366892d7e8c9a59bd5d7747ee7fe22223525
anked10 <angelo.anked@gmail.comm>
2022-03-04 16:38:39 -0500

    pusheado beibe db

--- lib/src/database/database_provider.dart
+++ lib/src/database/database_provider.dart
@@ -5,6 +5,98 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseProvider {
+  DatabaseProvider._privateConstructor();
+  static final DatabaseProvider db = DatabaseProvider._privateConstructor();
+
+  static Database? _database;
+  Future<Database> get database async => _database ??= await getDatabase();
+
+  Future<Database> getDatabase() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+
+    final path = join(documentsDirectory.path, 'ventas.db');
+
+
+    Future _onConfigure(Database db) async {
+      await db.execute('PRAGMA foreign_keys = ON');
+    }
+    
+     return openDatabase(
+      path,
+      onCreate: (db, version)async {
+       await db.execute(' CREATE TABLE Mesas('
+          ' idMesa TEXT PRIMARY KEY,'
+          ' idNegocio TEXT,'
+          ' mesaNombre TEXT,'
+          ' mesaCapacidad TEXT,'
+          ' mesaEstado TEXT,'
+          ' mesaEstadoAtencion TEXT'
+          ')');
+
+      await db.execute(' CREATE TABLE Familias('
+          ' idFamilia TEXT PRIMARY KEY,'
+          ' familiaNombre TEXT,'
+          ' familiaEstado TEXT'
+          ')');
+
+      await db.execute(' CREATE TABLE ProductosFamilia('
+          ' idProducto TEXT PRIMARY KEY,'
+          ' idFamilia TEXT,'
+          ' idReceta TEXT,'
+          ' productoNombre TEXT,'
+          ' productoDescripcion TEXT,'
+          ' productoFoto TEXT,'
+          ' productoPrecio TEXT,'
+          ' productoEstado TEXT'
+          ')');
+
+      await db.execute(' CREATE TABLE Comanda('
+          ' idComanda TEXT PRIMARY KEY,'
+          ' idMesa TEXT,'
+          ' total TEXT,'
+          ' fecha TEXT,'
+          ' estado TEXT'
+          ')');
+
+      await db.execute(' CREATE TABLE DetalleComandad('
+          ' idDetalle TEXT PRIMARY KEY,'
+          ' idComanda TEXT,'
+          ' idProducto TEXT,'
+          ' nombreProducto TEXT,'
+          ' fotoProducto TEXT,'
+          ' cantidad TEXT,'
+          ' subtotal TEXT,'
+          ' totalDetalle TEXT,'
+          ' observaciones TEXT,'
+          ' estado TEXT,'
+          ' llevar TEXT'
+          ')');
+
+      await db.execute(' CREATE TABLE DetalleComandaTemporal('
+          ' id INTEGER PRIMARY KEY AUTOINCREMENT,'
+          ' idMesa TEXT,'
+          ' idProducto TEXT,'
+          ' nombreProducto TEXT,'
+          ' fotoProducto TEXT,'
+          ' cantidad TEXT,'
+          ' subtotal TEXT,'
+          ' totalDetalle TEXT,'
+          ' observaciones TEXT,'
+          ' estado TEXT,'
+          ' despacho TEXT'
+          ')');
+
+      
+    
+      },
+      version: 1,
+      onConfigure: _onConfigure,
+    );
+  
+    }
+    }
+
+/* 
   static Database _database;
   static final DatabaseProvider db = DatabaseProvider._();
 
@@ -205,3 +297,4 @@ class DatabaseProvider {
     });
   }
 }
+ */

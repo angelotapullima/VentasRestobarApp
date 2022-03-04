61ef18fb196ee32a4dd80ed580fd39e2c3e75136
anked10 <angelo.anked@gmail.comm>
2022-03-04 17:17:10 -0500

    go

--- lib/src/database/database_provider.dart
+++ lib/src/database/database_provider.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
-
+//hwebvfikerwbfiouerbhfouerhgourgtouruog
 class DatabaseProvider {
   DatabaseProvider._privateConstructor();
   static final DatabaseProvider db = DatabaseProvider._privateConstructor();
